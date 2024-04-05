a23a8fabe3a4fd81a930aba2b08bca20109c3005
Tirna29 <tirna.adhikary@gmail.com>
2024-04-05 17:04:14 -0400

    Initial commit

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "TIRNA",
-  profile: '/tirna.png',
+  profile: '/tirna.jpg',
   designation: "Software Engineer",
   description: "I am TIRNA and as a Computer Science Graduate student at Rochester Institute of Technology, I thrive on problem-solving and coding, \
   with a focus on Software Development, Machine Learning, and Data Science. My career includes over 2 years of experience, notably as a Software Development \
@@ -11,5 +11,5 @@ export const personalData = {
   address: '220 John St, Rochester, NY, USA',
   github: 'https://github.com/Tirna29',
   linkedIn: 'https://www.linkedin.com/in/tirnaadhikary/',
-  resume: "https://drive.google.com/file/d/1iBspE3uV_1tAC4HUzzUrgwNPWfL4bnOg/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/1JHhd6mWy9DgWaz8T65WfZEFaZfaXz1lr/view?usp=sharing"
 }
