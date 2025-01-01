9ee7d198a8bb8392a256ae323a9fa88cd706076d
Tirna Adhikary <72922452+Tirna29@users.noreply.github.com>
2025-01-01 13:30:43 -0800

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,33 +1,33 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Teaching Assistant',
-    company: "Rochester Institute Of Technology",
-    duration: "(Aug 2023 - Apr 2024)",
-    description: "Mentored 150+ students in Artificial Intelligence and Cryptography, assessed assignments using GitHub, and demonstrated Java and Python \
-    based examples to clarify complex algorithms and cryptographic techniques."
+    title: 'Volunteer Software Engineer',
+    company: "CrowdDoing",
+    duration: "(Jul 2024 - Present)",
+    description: "Refactored the UI for the Nature Counter Android app using React Native and Android Studio, developing components to improve user experience.. Implemented APIs for managing \
+      journal entries, articles, and user profiles using Node.js and Redis."
   },
   {
     id: 2,
     title: 'Software Development Engineer Co-Op',
     company: "Amazon Robotics",
     duration: "(Jan 2023 - Jun 2023)",
-    description: "Developed a software emulator using Java and Kotlin to replicate the sequence of gantry movements of 100+ Fulfillment Center \
-    stations.. Engineered a simulation model with RESTful APIs and JSON configuration files to coordinate gantry movements, thereby reducing \
-    operation time by 43% to meet production performance requirements.. Managed component configurations with AWS S3 for storage and DynamoDB \
-    for metadata, simplifying the asset maintenance of the emulator.. Reduced post-deployment bugs by 50% through comprehensive Mockito integration tests and JUnit testing."
+    description: "Developed a software emulator using Java and Kotlin to replicate the sequence of gantry movements of 100+ Amazon Fulfillment Center stations.. Engineered a simulation model \
+      with RESTful APIs and JSON configuration files to coordinate gantry movements, thereby reducing operation time by 43% to meet production performance requirements.. Collaborated with 3 \
+      cross-functional teams to design and integrate modular Java components into the emulator, doubling development speed.. Managed component configurations with AWS S3 for storage and DynamoDB \
+      for metadata, improving the asset maintenance and asset accessibility for the emulator.. Reduced post-deployment bugs by 50% through comprehensive Mockito integration tests and JUnit \
+      testing.. Automated software deployment and maintenance using CI/CD pipelines, leveraging Git for version control, Docker for building containers, Kubernetes for container orchestration, and Gradle for building packages."
   },
   {
     id: 3,
     title: "Senior Systems Engineer",
     company: "Infosys",
     duration: "(Oct 2019 - Dec 2021)",
-    description: "Developed web applications using Java, HTML5, CSS3, JavaScript frameworks \
-    like React.. Built RESTful APIs with Java to integrate applications using a \
-    software-oriented architecture (SOA), resulting in streamlined integration of 50+ business processes.. Optimized \
-    SQL queries in MS SQL databases using clustered indexes and comprehensive database design, implementing \
-    stored procedures, triggers, and views, resulting in a 20% improvement in query speed.. Maintained 99% \
-    uptime by managing production support tickets and optimizing Azure-hosted applications through optimized storage, \
-    monitoring and diagnostics, meeting client SLAs and saving $40M annually."
+    description: "Redesigned six monolithic applications into microservices for a retail client, boosting scalability and performance while reducing downtime by 40%.. Implemented RESTful APIs using \
+      Spring Boot and JDBC with MySQL to streamline inventory management and receipt generation, boosting efficiency and reducing data processing time by 30%.. Optimized SQL queries in MySQL databases \
+      using clustered indexes and comprehensive database design, implementing stored procedures, triggers, and views, resulting in a 20% improvement in query execution speed.. Built responsive front-end \
+      applications using React and Redux, customized for specific business requirements, driving user engagement up by 2x through enhanced user experiences.. Created JUnit and integration tests to ensure \
+      high code quality and seamless API functionality, reducing production bugs by 35%.. Engineered automated CI/CD pipelines using Jenkins and Shell Scripting to integrate code, build JAR files, run JUnit \
+      tests, and deploy Docker containers, reducing deployment cycles by half and improving release reliability.. Deployed containerized microservices on Azure Kubernetes Service with Helm charts, achieving 99.9% uptime and increasing scalability by 3x."
   }
 ]
