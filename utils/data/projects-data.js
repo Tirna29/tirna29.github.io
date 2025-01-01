1af9de882c24ababcdae81899ed5d1e3e8e562de
Tirna Adhikary <72922452+Tirna29@users.noreply.github.com>
2025-01-01 13:39:51 -0800

    Update projects-data.js

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -6,33 +6,10 @@ import travel from '/public/image/travel.jpg';
 export const projectsData = [
     {
         id: 1,
-        name: 'Object Detection in Adverse Weather Conditions',
-        description: "Enhanced distorted, low-light images by leveraging the VOC 2007 dataset, developing a Differential Image Processing (DIP) module \
-        and a CNN. Optimized the DIP filter parameters through model training and improved the \
-        object detection capabilities of YOLOv3, achieving a mean average precision of 71%.",
-        tools: ['Python', 'PyTorch', 'NumPy', 'OpenCV','Linux', 'CNN'],
-        code: 'https://github.com/Tirna29/object-detection-in-adverse-weather-using-YOLOv3',
-        demo: '',
-        image: ayla,
-        role: '',
-    },
-    {
-        id: 2,
-        name: 'Underwater Image Enhancement Model',
-        description: "Developed within a Linux environment, this project utilized a Conditional GAN and a 90/10 training/validation split to enhance underwater images.\
-        Leveraged PyTorch for model development, achieving 82% Structural Similarity and 28% Peak Signal-to-Noise Ratio. Introduced early stopping regularization in training to prevent overfitting and improve model efficiency.",
-        tools: ['Linux', 'Python', 'PyTorch', 'Scipy', 'Numpy', 'Pillow'],
-        code: 'https://github.com/Tirna29/enhancing-underwater-images-using-GAN',
-        demo: '',
-        image: ayla,
-        role: '',
-    },
-    {
-        id: 3,
-        name: 'Generalizing Graph HyperNetworks',
-        description: "Pioneered GHN-2 adaptation for unseen datasets using curriculum learning, eliminating retraining needs by using pre-trained GHN-2 weights for initializing DNN models such as \
-        Resnet-50 and Resnet-50-2. Optimized with \
-        CIFAR-10 and CIFAR-100, thereby reducing the training time by 16% and achieving a breakthrough 84.85% accuracy on DNNs.",
+        name: 'Improving DNN Initialization with Curriculum Learning',
+        description: "Enhanced the efficiency of deep neural network (DNN) training by initializing models with GHN-2 weights using transfer learning and \
+            curriculum learning to dynamically adapt the model to new datasets. Boosted ResNet-50 performance on the CIFAR-100 dataset by achieving a 25% accuracy \
+            improvement and reducing training epochs by 50% compared to training the model from scratch.",
         tools: ['Linux', 'Python', 'PyTorch', 'Matplotlib', 'Scikit-learn', 'NumPy', 'Pandas', 'CNN'],
         code: '',
         demo: '',
@@ -40,7 +17,45 @@ export const projectsData = [
         role: '',
     },
     {
+        id: 2,
+        name: 'Object Detection in Adverse Weather Conditions',
+        description: "Created an object detection system using an Image processing module, a custom CNN, and YOLOv3 using PyTorch, NumPy, Matplotlib, and OpenCV, \
+            resulting in adaptive object detection in adverse weather conditions. Optimized filter parameters of the image processing module by tuning the CNN based \
+            on YOLOv3 output, achieving a 71% mean average precision in foggy and low-light images.",
+        tools: ['Python', 'PyTorch', 'NumPy', 'OpenCV','Linux', 'CNN'],
+        code: 'https://github.com/Tirna29/object-detection-in-adverse-weather-using-YOLOv3',
+        demo: '',
+        image: ayla,
+        role: '',
+    },
+    {
+        id: 3,
+        name: 'Underwater Image Enhancement Model',
+        description: "Built a conditional GAN-based model using PyTorch, NumPy, and SciPy, employing a perceptual loss function to optimize color, texture, and \
+            style for improved underwater image quality. Achieved 82% Structural Similarity and 28% Peak Signal-to-Noise Ratio through model training on the EUVP \
+            image dataset, outperforming state-of-the-art models in qualitative and quantitative evaluations.",
+        tools: ['Linux', 'Python', 'PyTorch', 'Scipy', 'Numpy', 'Pillow'],
+        code: 'https://github.com/Tirna29/enhancing-underwater-images-using-GAN',
+        demo: '',
+        image: ayla,
+        role: '',
+    },
+    
+    {
         id: 4,
+        name: 'Netflix Recommendation System',
+        description: 'Developed a content-based movie recommendation system using Python, Matplotlib, Scikit-learn, NumPy, PyCharm, and Pandas. Achieved high content matching \
+            accuracy and a 10% improvement in recommendations by leveraging a KNN classifier. Utilized MongoDB for data storage and derived actionable insights from data \
+            analysis. Applied data visualization techniques such as Word Clouds to gain insights and enhance recommendation accuracy.',
+        tools: ['Python', 'MongoDB', 'Data Analysis', 'Machine Learning', 'NumPy'],
+        code: 'https://github.com/Tirna29/netflix-recommendation-system',
+        role: '',
+        demo: '',
+        image: realEstate,
+        
+    },
+    {
+       id: 5,
         name: 'Real Time Market Insights Dashboard',
         description: 'Developed a real-time analytics dashboard for financial markets, using open-source datasets and GraphQL for optimized data retrieval.\
         Employed React and the FusionCharts Suite XT JavaScript library for the frontend, crafting a responsive interface that enhances live market data visualization.',
@@ -49,19 +64,6 @@ export const projectsData = [
         role: '',
         demo: '',
         image: realEstate,
-        
-    },
-    {
-        id: 5,
-        name: 'Package Delivery Application',
-        description: "Managed a team of 4 in creating a package delivery website, consisting of features such as package booking, dual login capabilities, \
-        and accurate cost estimation. Implemented the backend using ASP.NET to create RESTful APIs in C#, complemented by \
-        Entity Framework Core for data access and PostgreSQL for database management.",
-        tools: ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'C#', 'ASP.NET', 'Entity Framework Core'],
-        role: '',
-        code: '',
-        demo: '',
-        image: crefin,
         
     }  
 
