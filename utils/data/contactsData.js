c96406a8d66ecee0c4235c98f42c21928b220f78
Tirna Adhikary <72922452+Tirna29@users.noreply.github.com>
2025-01-01 13:19:16 -0800

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
     email: 'ta5900@rit.edu',
-    phone: '+5859107695',
+    phone: '+1-585-910-7695',
     address: 'San Jose, California, USA',
     github: 'https://github.com/Tirna29',
     linkedIn: 'https://www.linkedin.com/in/tirnaadhikary/'
